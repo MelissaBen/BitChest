470e08fc6a6ee8552c49ae8f02c198ed8898023e
Melissa Ben <melissabenmeziane01@gmail.com>
2021-08-30 23:34:50 +0200

    Clean Up

--- public/js/app.js
+++ public/js/app.js
@@ -75468,19 +75468,7 @@ var staticRenderFns = [
     var _c = _vm._self._c || _h
     return _c("div", { staticClass: "container" }, [
       _c("div", { staticClass: "row justify-content-center" }, [
-        _c("div", { staticClass: "col-md-8" }, [
-          _c("div", { staticClass: "card" }, [
-            _c("div", { staticClass: "card-header" }, [
-              _vm._v("Example Component")
-            ]),
-            _vm._v(" "),
-            _c("div", { staticClass: "card-body" }, [
-              _vm._v(
-                "\n                    I'm an example component.\n                "
-              )
-            ])
-          ])
-        ])
+        _c("div", { staticClass: "col-md-8" })
       ])
     ])
   }
